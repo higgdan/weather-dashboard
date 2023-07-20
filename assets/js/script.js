3f54e318953585c5bde411c3ef154893f50dea23
danhigg <higgins.daniel@gmail.com>
2023-07-20 22:11:41 +1000

    added current/forecast create element function

--- assets/js/script.js
+++ assets/js/script.js
@@ -44,3 +44,54 @@ let getFiveDayForecast = function(apiURL) {
             constructFiveDayForecast(data);
         });
 }
+
+let constructCurrentWeather = function(data) {
+    let cityNameEl = document.createElement("h2");
+    cityNameEl.innerText = data.name;
+    let dateEl = document.createElement("h3");
+    dateEl.innerText = dayjs.unix(data.dt).format("dddd, MMM D, YYYY");
+    let imgEl = document.createElement("img");
+    let imgURL = "https://openweathermap.org/img/wn/"+ data.weather[0].icon  +"@2x.png";
+    imgEl.setAttribute("src", imgURL);
+    imgEl.setAttribute("alt", data.weather[0].description);
+    let tempEl = document.createElement("h4");
+    tempEl.innerText = "Temp: " + Math.round(data.main.temp) + "°C";
+    let humidityEl = document.createElement("h4");
+    humidityEl.innerText = "Humidity: " + data.main.humidity + "%";
+    let windEl = document.createElement("h4");
+    windEl.innerText = "Wind: " + Math.round(data.wind.speed * 3.6) + "km/h";
+
+    currentWeather.appendChild(cityNameEl);
+    currentWeather.appendChild(dateEl);
+    currentWeather.appendChild(imgEl);
+    currentWeather.appendChild(tempEl);
+    currentWeather.appendChild(humidityEl);
+    currentWeather.appendChild(windEl);
+}
+
+let constructFiveDayForecast = function(data) {
+    for (let i = 1; i <= 5; i ++) {
+        let index = ((i*8)-1);
+        let dayDiv = document.createElement("div");
+        dayDiv.setAttribute("id", "single-day-forecast");
+        let dateEl = document.createElement("h3");
+        dateEl.innerText = dayjs(data.list[index].dt_txt).format("dddd");
+        let imgEl = document.createElement("img");
+        let imgURL = "https://openweathermap.org/img/wn/"+ data.list[index].weather[0].icon  +".png";
+        imgEl.setAttribute("src", imgURL);
+        imgEl.setAttribute("alt", data.list[index].weather[0].description);
+        let tempEl = document.createElement("h4");
+        tempEl.innerText = "Temp: " + Math.round(data.list[index].main.temp) + "°C";
+        let humidityEl = document.createElement("h4")
+        humidityEl.innerText = "Humidity: " + data.list[index].main.humidity + "%";
+        let windEl = document.createElement("h4");
+        windEl.innerText = "Wind: " + Math.round(data.list[index].wind.speed * 3.6) + "km/h";
+
+        dayDiv.appendChild(dateEl);
+        dayDiv.appendChild(imgEl);
+        dayDiv.appendChild(tempEl);
+        dayDiv.appendChild(humidityEl);
+        dayDiv.appendChild(windEl);
+        fiveDayForecast.appendChild(dayDiv);
+    }
+}
