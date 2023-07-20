4f811060da355b3f411848ab7e23aa997f33d3a0
danhigg <higgins.daniel@gmail.com>
2023-07-20 22:09:40 +1000

    added fetch API functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,46 @@
-console.log("hellooooo");
-
 let weatherKey = "327ee92c9d8ab9ee1c3b847e7bac81e7"
 
-// api.openweathermap.org/data/2.5/forecast?q=München,DE&appid={API key}
-// http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
-
+let forecastURL = "";
 let cityLat = ""; // -37.8142176
 let cityLong = ""; // 144.9631608
 
 
-let requestUrl = "http://api.openweathermap.org/geo/1.0/direct?q=Melbourne&limit=5&appid=" + weatherKey
+let getCityCoords = function() {
+    let coordRequestURL = "http://api.openweathermap.org/geo/1.0/direct?q="+ currentCitySearch +"&limit=5&appid=" + weatherKey;
+    fetch(coordRequestURL)
+        .then(function (response) {
+        return response.json();
+        })
+        .then(function (data) {
+            cityLat = data[0].lat;
+            cityLong = data[0].lon;
+            defineWeatherURLS(cityLat,cityLong);
+        });
+}
 
-let getCoords = fetch(requestUrl)
-    .then(function (response) {
-    return response.json();
-    })
-    .then(function (data) {
-        console.log(data);
-        console.log(data[0].state);
-        cityLat = data[0].lat;
-        cityLong = data[0].lon;
-        console.log(cityLat,cityLong);
-        return;
-    });
+let defineWeatherURLS = function(a,b) {
+        weatherURL = "http://api.openweathermap.org/data/2.5/weather?lat="+ a + "&lon=" + b +"&units=metric&appid=" + weatherKey
+        forecastURL = "http://api.openweathermap.org/data/2.5/forecast?lat="+ a + "&lon=" + b +"&units=metric&appid=" + weatherKey
+        getCurrentWeather(weatherURL);
+        getFiveDayForecast(forecastURL);
+}
 
-console.log(cityLat);
+let getCurrentWeather = function(apiURL) {
+    fetch(apiURL)
+        .then(function (response) {
+        return response.json();
+        })
+        .then(function (data) {
+            constructCurrentWeather(data);
+        });
+}
+
+let getFiveDayForecast = function(apiURL) {
+    fetch(apiURL)
+        .then(function (response) {
+        return response.json();
+        })
+        .then(function (data) {
+            constructFiveDayForecast(data);
+        });
+}
